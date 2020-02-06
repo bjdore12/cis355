8f1944e74032dd6ca500b8d88782514312696382
Benjamin <bjdore@svsu.edu>
2020-02-06 18:35:43 -0500

    Adding links to assignments

--- cis355.php
+++ cis355.php
@@ -1,9 +1,8 @@
 <?php
 
     echo "<h1>Benjamin Dore - CIS 355</h1>";
-
     echo "<br>";
-
-    echo "<a>Assignment 1</a><br>";
-    echo "<a>Assignment 2</a><br>";
+    echo "<a href='http://bjdore.000webhostapp.com'>Assignment 1</a> - Introductory assignment to make sure things work and to get familiar with PHP<br>";
+    echo "<a href=As02/display_list.php>Assignment 2</a> - Playing around with MySQL and PHP, create a CRUD app<br>";
+    
 ?>
