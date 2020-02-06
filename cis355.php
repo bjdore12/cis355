56f62ddd932eec5c7381c8280807d6cec4eff49f
bjdore12 <45237685+bjdore12@users.noreply.github.com>
2020-02-06 18:44:37 -0500

    Adding comment to main page

--- cis355.php
+++ cis355.php
@@ -1,5 +1,7 @@
 <?php
 
+    # Echo statements to display links to assignments
+
     echo "<h1>Benjamin Dore - CIS 355</h1>";
     echo "<br>";
     echo "<a href='http://bjdore.000webhostapp.com'>Assignment 1</a> - Introductory assignment to make sure things work and to get familiar with PHP<br>";
